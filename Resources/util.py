aee259cc69a15b76c1b162c8d1ed1872f29486ad
Anthony <Frenchy2199@yahoo.com>
2022-02-07 21:08:47 -0700

    WE GONNA BE SPEEDY GONZALEZ WITH THESE EFFECIENT LOOKUPS MY DUDE

--- Resources/util.py
+++ Resources/util.py
@@ -1,5 +1,10 @@
 import requests
+import os
+
 from requests.api import request
+from datetime import date, datetime
+
+from Resources.Items import Item_Dict
 
 api_url = "https://services.runescape.com/m=itemdb_rs/api/catalogue/"
 
@@ -35,19 +40,52 @@ def price_to_str(price):
         return (str(million) + "." + str(thousand) + "m", str(thousand) + "." + str(price) + "k") [million == 0]
 
 def retrieve_item(name, category):
+    if name not in Item_Dict.keys():
+        # add this item in our dictionary
+        item = search_item(name, category)
+        add_item(name, item)
+        return item 
+
+    else:
+        # two scenario here, key exists and is up to date, key exists and is old
+        item = Item_Dict[name]
+        if(item['date'] == date.isoformat(datetime.utcnow().date())):
+            # woo we don't do an API call
+            return item
+        else:
+            # damn still have to do an API call
+            item = search_item(name, category)
+            add_item(name, item)
+            return item
+            
+
+def search_item(name, category):
     found = False
     page = 0
 
     while found != True:
-            #TODO: error proof response from timeouts and other things that would make my code oopsie woopsie
-            response = requests.get(api_url + "items.json?category=" + str(category) + "&alpha=" + name[0].lower() + "&page=" + str(page))
-            
-            if len(response.json()['items']) == 0:
-                # items are empty, we have exhausted the list
-                raise NameError(name + " not found")
-            else:
-                # Search result for given item
-                for item in response.json()['items']:
-                    if item['name'] == name:
-                        return item
-            page += 1
+        #TODO: error proof response from timeouts and other things that would make my code oopsie woopsie
+        response = requests.get(api_url + "items.json?category=" + str(category) + "&alpha=" + name[0].lower() + "&page=" + str(page))
+        
+        if len(response.json()['items']) == 0:
+            # items are empty, we have exhausted the list
+            raise NameError(name + " not found")
+        else:
+            # Search result for given item
+            for item in response.json()['items']:
+                if item['name'] == name:
+                    return item
+        page += 1
+
+def add_item(name, item):
+    lines = []
+    item['date'] = str_today = date.isoformat(datetime.utcnow().date())
+    with open("Resources/Items.py", "r", encoding = "utf-8") as f:
+        lines = f.readlines()
+        lines = lines[:-1]  # remove last line since we're appending to the dictionary
+    with open("Resources/Items.py", "w", encoding="utf-8") as f:
+        for line in lines:
+            f.write(line)
+        f.write("\'" + name + "\': " + str(item) + ",\n")
+        f.write("}")
+        f.close()
